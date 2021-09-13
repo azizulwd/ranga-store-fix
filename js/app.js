7a7545c180c4cfc39a1709c97ac4264c265c72c2
azizulwd <azizulislamgd@gmail.com>
2021-09-14 02:12:08 +0600

    update rating

--- js/app.js
+++ js/app.js
@@ -18,8 +18,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <p id="rating_count">${product.rating.count} people like this.</p>
-      <p id="rating_average">Average rating ${product.rating.rate}</p>
+      <p id="rating_count">Rating Count: ${product.rating.count}</p>
+      <p id="rating_average">Rating: ${product.rating.rate}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
