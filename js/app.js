4748dcab4e0c5e6642420484dbb99e05da6f5823
azizulwd <azizulislamgd@gmail.com>
2021-09-14 02:42:44 +0600

    update design

--- js/app.js
+++ js/app.js
@@ -18,8 +18,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <p id="rating_count">Rating Count: ${product.rating.count}</p>
-      <p id="rating_average">Rating: ${product.rating.rate}</p>
+      <p style="color: green;" id="rating_count">Rating Count: ${product.rating.count}</p>
+      <p style="color: orange;" id="rating_average">Rating: ${product.rating.rate}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
